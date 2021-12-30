c3211540a0f175100d249502f451366d10db663f
RoySamson-stack <rsam9009@gmail.com>
2021-12-30 17:46:19 +0300

    adding ejs and updating the app.js file

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const bodyParser = require('body-parser');
 
-
 const app = express();
+
+
+app.set('view engine', 'ejs');
 
 app.get("/", function (req, res) {
   var today = new Date();
