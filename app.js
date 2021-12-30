03186f9ddba5d2320e086ee0634a7d855164daa2
RoySamson-stack <rsam9009@gmail.com>
2021-12-30 17:30:25 +0300

    adding the information to the weekend and weekday html files

--- app.js
+++ app.js
@@ -5,18 +5,19 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get("/", function(req, res){
-    var currentDay = today.getDay();
+app.get("/", function (req, res) {
+  var today = new Date();
+  var currentDay = today.getDay();
 
 
-    if(currentDay == 6 ||currentDay == 0){
-      res.write("<h1>Yay its a weekend</h1>")
-    }else{
-      res.sendFile(__dirname + "/index.html");
-    }
+  if (currentDay == 6 || currentDay == 0) {
+    res.sendFile(__dirname + "weekend.html")
+  } else {
+    res.sendFile(__dirname + "/weekday.html");
+  }
 })
 
 //app listen 
-app.listen(3000, function(){
-    console.log("Server is running on port 3000");
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
 });
