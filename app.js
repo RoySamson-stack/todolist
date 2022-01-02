08b88b1ecfaedf82a13e587c4d01507eb0a91407
RoySamson-stack <rsam9009@gmail.com>
2022-01-02 11:11:53 +0300

    adding the styles to the todolist

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
       var today = new Date();
@@ -19,9 +22,18 @@ app.get("/", function (req, res) {
 
       var day = today.toLocaleDateString("en-US", options);
           res.render("list", {
-          kindOfDay: day
+          kindOfDay: day,
+          newListItems:items,
           })
 
+      })
+
+      app.post("/", (req, res) =>{
+        var item = req.body.newItem;
+
+        items.push(item);
+
+        res.redirect("/", )
       })
     app.listen(5500, function () {
       console.log("Server is running on port 5500");
