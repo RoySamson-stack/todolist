e9edfd8c7df5b1a77c71b26a66cea6e4eefc21be
RoySamson-stack <rsam9009@gmail.com>
2022-01-16 12:26:18 +0300

    finishing the app

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-roy:riot9332@cluster0.nilyc.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
@@ -128,12 +128,13 @@ app.post("/delete", function(req, res){
     });
   }
 
-
 });
+
 
 app.get("/about", function(req, res){
   res.render("about");
 });
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
