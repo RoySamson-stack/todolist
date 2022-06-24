c231fbd63953915de19c2331736393f394cc6f43
RoySamson-stack <rsam9009@gmail.com>
2022-06-24 22:21:31 +0300

    update

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-roy:riot9332@cluster0.nilyc.mongodb.net/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
@@ -22,15 +22,15 @@ const Item = mongoose.model("Item", itemsSchema);
 
 
 const item1 = new Item({
-  name: "bread"
+  name: "Welcome to your todolist!"
 });
 
 const item2 = new Item({
-  name: "milk"
+  name: "Hit the + button to add a new item."
 });
 
 const item3 = new Item({
-  name: "eggs"
+  name: "<-- Hit this to delete an item."
 });
 
 const defaultItems = [item1, item2, item3];
@@ -128,18 +128,13 @@ app.post("/delete", function(req, res){
     });
   }
 
-});
 
+});
 
 app.get("/about", function(req, res){
   res.render("about");
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-
-app.listen(port, function() {
-  console.log("Server has successfully started");
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
